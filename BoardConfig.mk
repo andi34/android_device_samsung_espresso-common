9043e1d34ea8d575686e61c16e883e49f156077a
Andreas Blaesius <skate4life@gmx.de>
2017-11-29 21:56:23 +0100

    Device specific TWRP needs own defconfig, we can share most parts

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,76 +18,8 @@
 # This variable is set first, so it can be overridden
 # by BoardConfigVendor.mk
 
-TARGET_NO_BOOTLOADER := true
-
-# Processor
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_VARIANT := cortex-a9
-TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_SMP := true
-TARGET_ARCH := arm
-TARGET_BOARD_PLATFORM := omap4
-TARGET_BOARD_PLATFORM_VARIANT := omap4-next
-TARGET_BOARD_OMAP_CPU := 4430
-TARGET_ARCH_VARIANT := armv7-a-neon
-
-TARGET_BOOTLOADER_BOARD_NAME := piranha
+include device/samsung/espressocommon/BoardConfigCommon.mk
 
 # Inline kernel building
 TARGET_KERNEL_SOURCE := kernel/ti/omap4
 TARGET_KERNEL_CONFIG := espresso_defconfig
-BOARD_NAND_PAGE_SIZE := 4096
-BOARD_NAND_SPARE_SIZE := 128
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_BASE := 0x40000000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=espresso
-
-# Filesystem
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
-BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
-BOARD_CACHEIMAGE_PARTITION_SIZE := 734003200
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
-BOARD_FLASH_BLOCK_SIZE := 4096
-
-# Vold
-BOARD_VOLD_MAX_PARTITIONS := 12
-BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
-BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/f_mass_storage/lun%d/file"
-
-# Misc
-BOARD_USES_SECURE_SERVICES := true
-
-# Selinux
-BOARD_SEPOLICY_DIRS += device/samsung/espressowifi/sepolicy
-BOARD_SEPOLICY_DIRS += device/samsung/espresso3g/sepolicy
-BOARD_SEPOLICY_DIRS += hardware/ti/omap4/sepolicy
-
-# Recovery
-TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
-BOARD_UMS_LUNFILE := "/sys/class/android_usb/f_mass_storage/lun0/file"
-TARGET_RECOVERY_FSTAB := device/samsung/espressowifi/rootdir/fstab.espresso
-RECOVERY_FSTAB_VERSION := 2
-TARGET_RECOVERY_DEVICE_DIRS += device/samsung/espressowifi
-LZMA_RAMDISK_TARGETS := recovery
-
-# TWRP
-TW_THEME := landscape_hdpi
-HAVE_SELINUX := true
-RECOVERY_SDCARD_ON_DATA := true
-BOARD_HAS_NO_REAL_SDCARD := true
-TW_INTERNAL_STORAGE_PATH := "/data/media"
-TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
-TW_EXTERNAL_STORAGE_PATH := "/external_sd"
-TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
-TW_NO_REBOOT_BOOTLOADER := true
-TW_HAS_DOWNLOAD_MODE := true
-TW_FLASH_FROM_STORAGE := true
-TW_INCLUDE_CRYPTO := true
-TW_MAX_BRIGHTNESS := 255
-TW_NO_CPU_TEMP := true
-TW_EXCLUDE_DEFAULT_USB_INIT := true
-TW_EXCLUDE_SUPERSU := true
