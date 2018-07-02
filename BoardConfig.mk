d2e811e2f868eaa7599902b6df97d3c8bd095d45
Andreas Blaesius <andi@unlegacy-android.org>
2018-07-02 12:18:52 +0200

    use common kernel defconfig
    Change-Id: Ida67a2267e10c5633bcf929c0f3c00d98eedc052

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,4 @@
 include device/samsung/espressocommon/BoardConfigCommon.mk
 
 # Inline kernel building
-TARGET_KERNEL_SOURCE := kernel/ti/omap4
-TARGET_KERNEL_CONFIG := espresso_defconfig
 VARIANT_DEFCONFIG := p3100_defconfig
