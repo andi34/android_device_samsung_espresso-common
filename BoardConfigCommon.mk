34d0c00c642e46f31d87d46266e11469c61d327c
Andreas Blaesius <andi@unlegacy-android.org>
2019-02-19 21:35:08 +0100

    Switch back to normal defconfig for now

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -39,7 +39,7 @@ KERNEL_TOOLCHAIN := "$(ANDROID_BUILD_TOP)/prebuilts/gcc/$(strip $(HOST_OS))-x86/
 
 # Inline kernel building
 TARGET_KERNEL_SOURCE := kernel/ti/omap4
-TARGET_KERNEL_CONFIG := espresso_recovery_defconfig
+TARGET_KERNEL_CONFIG := espresso_defconfig
 BOARD_NAND_PAGE_SIZE := 4096
 BOARD_NAND_SPARE_SIZE := 128
 BOARD_KERNEL_PAGESIZE := 2048
